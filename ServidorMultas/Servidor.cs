7269d0c178b3430354ecccea9d936d83d6f4e4c8
Rubén González Membrilla <32097615+RGM93@users.noreply.github.com>
2017-09-20 15:52:55 +0200

    Add files via upload

---
+++ ServidorMultas/Servidor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicioDGTMultas
+{
+    class Servidor
+    {
+        static void Main(string[] args)
+        {
+            /*ChannelServices.RegisterChannel(new TcpChannel(12345), false);
+            Console.WriteLine("Registrando el servicio de la DGT Remota...");
+            RemotingConfiguration.RegisterWellKnownServiceType(typeof(ServicioMultas), "ServicioDGTMultas", WellKnownObjectMode.Singleton);*/
+            RemotingConfiguration.Configure("ServidorMultas.exe.config", false);
+
+            Console.WriteLine("Esperando llamadas Remotas...");
+            Console.WriteLine("Pulsa Enter para Salir..");
+            Console.ReadLine();
+
+        }
+    }
+}
