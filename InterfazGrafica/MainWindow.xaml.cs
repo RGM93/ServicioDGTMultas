8556121ee2031eb42af13ae32e4894fb46249844
Rubén González Membrilla <32097615+RGM93@users.noreply.github.com>
2017-09-20 21:02:16 +0200

    Add files via upload

---
+++ InterfazGrafica/MainWindow.xaml.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ServicioDGTMultas;
+using ComunDGTMultas;
+
+namespace InterfazGrafica
+{
+    public partial class MainWindow : Window
+    {
+        
+        public ServicioMultas s = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            id.Visibility = System.Windows.Visibility.Hidden;
+            cP.Visibility = System.Windows.Visibility.Hidden;
+            cM.Visibility = System.Windows.Visibility.Hidden;
+            resultado.Visibility = System.Windows.Visibility.Hidden;
+            Tabla.Visibility = System.Windows.Visibility.Hidden;
+            nPuntos.Visibility = System.Windows.Visibility.Hidden;
+            //ChannelServices.RegisterChannel(new TcpChannel(), false);
+            //this.s = (ServicioMultas)Activator.GetObject(typeof(ServicioMultas), "tcp://localhost:12345/ServicioMultas");
+            RemotingConfiguration.Configure("InterfazGrafica.exe.config", false);
+            s = new ServicioMultas();
+        }
+
+        public MainWindow(ServicioMultas s){
+            InitializeComponent();
+            id.Visibility = System.Windows.Visibility.Hidden;
+            cP.Visibility = System.Windows.Visibility.Hidden;
+            cM.Visibility = System.Windows.Visibility.Hidden;
+            resultado.Visibility = System.Windows.Visibility.Hidden;
+            Tabla.Visibility = System.Windows.Visibility.Hidden;
+            nPuntos.Visibility = System.Windows.Visibility.Hidden;
+            this.s = s;
+        }
+
+        private void identificacionBoton(object sender, RoutedEventArgs e)
+        {
+            cP.Visibility = System.Windows.Visibility.Hidden;
+            cM.Visibility = System.Windows.Visibility.Hidden;
+            id.Visibility = System.Windows.Visibility.Visible;
+            resultado.Visibility = System.Windows.Visibility.Hidden;
+            Tabla.Visibility = System.Windows.Visibility.Hidden;
+            nPuntos.Visibility = System.Windows.Visibility.Hidden;
+        }
+
+        private void comprobarPuntosBoton(object sender, RoutedEventArgs e)
+        {
+            cM.Visibility = System.Windows.Visibility.Hidden;
+            id.Visibility = System.Windows.Visibility.Hidden;
+            cP.Visibility = System.Windows.Visibility.Visible;
+            resultado.Visibility = System.Windows.Visibility.Hidden;
+            Tabla.Visibility = System.Windows.Visibility.Hidden;
+            nPuntos.Visibility = System.Windows.Visibility.Hidden;
+        }
+
+        private void comprobarMultasBoton(object sender, RoutedEventArgs e)
+        {
+            id.Visibility = System.Windows.Visibility.Hidden;
+            cP.Visibility = System.Windows.Visibility.Hidden;
+            cM.Visibility = System.Windows.Visibility.Visible;
+            resultado.Visibility = System.Windows.Visibility.Hidden;
+            Tabla.Visibility = System.Windows.Visibility.Hidden;
+            nPuntos.Visibility = System.Windows.Visibility.Hidden;
+        }
+
+
+        private void salir(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void comprobarIdentificacionBoton(object sender, RoutedEventArgs e)
+        {
+            MainWindows2 m;
+            String idP = "";
+            idP = Convert.ToString(boxMatricula.Text);
+            int result = 0;
+            result = s.Identificacion(idP);
+
+            if (result == 1)
+            {
+                m = new MainWindows2(s);
+                m.Visibility = System.Windows.Visibility.Visible;
+                this.Visibility = System.Windows.Visibility.Hidden;
+            }
+        }
+
+        private void comprobarPuntosBoton2(object sender, RoutedEventArgs e)
+        {
+            int d; String m;
+            d = Convert.ToInt32(boxDP.Text); m = Convert.ToString(boxMP.Text);
+
+            int result = s.ComprobarPuntos(d, m);
+
+            if (result != -1)
+            {
+                cP.Visibility = System.Windows.Visibility.Hidden;
+                resultado.Visibility = System.Windows.Visibility.Visible;
+                t.Text = Convert.ToString(result);
+                //t.Visibility = System.Windows.Visibility.Visible;
+;           }
+        }
+
+        private void comprobarMultasBoton2(object sender, RoutedEventArgs e)
+        {
+            LinkedList<DatosSancion> ds = null;
+            String m; int d;
+            m = Convert.ToString(boxMP2.Text);
+            d = Convert.ToInt32(boxDP2.Text);
+            ds = s.ComprobarMultas(d, m);
+
+            if (ds.Count != 0)
+            {
+                Tabla.ItemsSource = ds;
+                nPuntos.Text = "Numero de Multas: " + Convert.ToString(ds.Count);
+                
+            }
+
+            Tabla.Visibility = System.Windows.Visibility.Visible;
+            nPuntos.Visibility = System.Windows.Visibility.Visible;
+            id.Visibility = System.Windows.Visibility.Hidden;
+            cP.Visibility = System.Windows.Visibility.Hidden;
+            cM.Visibility = System.Windows.Visibility.Hidden;
+            resultado.Visibility = System.Windows.Visibility.Hidden;
+
+
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ InterfazGrafica/MainWindows2.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ServicioDGTMultas;
+
+namespace InterfazGrafica
+{
+    public partial class MainWindows2 : Window
+    {
+        ServicioMultas s = null;
+        public MainWindows2(ServicioMultas s)
+        {
+            InitializeComponent();
+            pM.Visibility = System.Windows.Visibility.Hidden;
+            qM.Visibility = System.Windows.Visibility.Hidden;
+            aV.Visibility = System.Windows.Visibility.Hidden;
+            bV.Visibility = System.Windows.Visibility.Hidden;
+            this.s = s;
+        }
+
+        private void ponerMultaBoton(object sender, RoutedEventArgs e)
+        {
+            pM.Visibility = System.Windows.Visibility.Visible;
+            qM.Visibility = System.Windows.Visibility.Hidden;
+            aV.Visibility = System.Windows.Visibility.Hidden;
+            bV.Visibility = System.Windows.Visibility.Hidden;
+        }
+
+        private void quitarMultaBoton(object sender, RoutedEventArgs e)
+        {
+            pM.Visibility = System.Windows.Visibility.Hidden;
+            qM.Visibility = System.Windows.Visibility.Visible;
+            aV.Visibility = System.Windows.Visibility.Hidden;
+            bV.Visibility = System.Windows.Visibility.Hidden;
+        }
+
+        private void altaVehiculoBoton(object sender, RoutedEventArgs e)
+        {
+            pM.Visibility = System.Windows.Visibility.Hidden;
+            qM.Visibility = System.Windows.Visibility.Hidden;
+            aV.Visibility = System.Windows.Visibility.Visible;
+            bV.Visibility = System.Windows.Visibility.Hidden;
+        }
+
+        private void bajaVehiculoBoton(object sender, RoutedEventArgs e)
+        {
+            pM.Visibility = System.Windows.Visibility.Hidden;
+            qM.Visibility = System.Windows.Visibility.Hidden;
+            aV.Visibility = System.Windows.Visibility.Hidden;
+            bV.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        private void volverBoton(object sender, RoutedEventArgs e)
+        {
+            MainWindow m = new MainWindow(s);
+            m.Visibility = System.Windows.Visibility.Visible;
+            this.Visibility = System.Windows.Visibility.Hidden;
+        }
+
+        private void botonMultar(object sender, RoutedEventArgs e)
+        {
+            String m, f; int p;
+            m = Convert.ToString(boxM1.Text); f = Convert.ToString(boxF1.Text); p = Convert.ToInt32(boxP1.Text);
+            s.PonerMulta(m, f, p);
+        }
+
+        private void botonQuitar(object sender, RoutedEventArgs e)
+        {
+            String m, f;
+            m = Convert.ToString(boxM2.Text); f = Convert.ToString(boxF2.Text);
+            s.QuitarMulta(m, f);
+        }
+
+        private void botonAlta(object sender, RoutedEventArgs e)
+        {
+            int d; String m;
+            d = Convert.ToInt32(boxD3.Text); m = Convert.ToString(boxM3.Text);
+            s.AltaVehiculo(d, m);
+        }
+
+        private void botonBaja(object sender, RoutedEventArgs e)
+        {
+            int d; String m;
+            d = Convert.ToInt32(boxD4.Text); m = Convert.ToString(boxM4.Text);
+            s.BajaVehiculo(d, m);
+        }
+
+
+    }
+}
